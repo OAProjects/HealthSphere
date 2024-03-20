5f89123ab6e5a8dfaaca19e51d62aff994f6e384
Arda Sahan <ardambp@outlook.com>
2024-03-20 22:24:59 +0000

    created the first iteration of the register which works with the backend to create new users

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -3,9 +3,45 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import background from "../../assets/background.png";
+import axios from "axios";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 import styles from "./Register.module.css";
 
+const registerSchema = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  password: Yup.string()
+    .min(6, "Password must contain at least 6 characters")
+    .required("Required"),
+});
+
 const Register = () => {
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      role: "patient", // Default role to patient
+    },
+    validationSchema: registerSchema,
+    onSubmit: async (values) => {
+      try {
+        // Perform registration logic
+        await axios.post("http://localhost:3001/auth/register", values);
+
+        // Redirect to the login page after successful registration
+        navigate("/login");
+      } catch (error) {
+        console.error("Registration failed:", error.message);
+      }
+    },
+  });
+
   return (
     <section
       style={{
@@ -17,41 +53,84 @@ const Register = () => {
       <div className={styles.container}>
         <div className={styles.panel}>
           <h1>Register</h1>
-          <Form className={styles.form}>
+          <Form className={styles.form} onSubmit={formik.handleSubmit}>
             <Form.Group as={Row} className="mb-3">
               <Col sm={10}>
                 <Form.Check
                   type="radio"
                   label="Patient"
-                  name="formHorizontalRadios"
-                  id="formHorizontalRadios1"
+                  name="role"
+                  value="patient"
+                  id="role-patient"
+                  checked={formik.values.role === "patient"}
+                  onChange={formik.handleChange}
                 />
                 <Form.Check
                   type="radio"
                   label="Doctor"
-                  name="formHorizontalRadios"
-                  id="formHorizontalRadios2"
+                  name="role"
+                  value="doctor"
+                  id="role-doctor"
+                  checked={formik.values.role === "doctor"}
+                  onChange={formik.handleChange}
                 />
               </Col>
             </Form.Group>
             <Form.Group>
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your full name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter your full name"
+                name="firstName"
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.firstName && formik.errors.firstName && (
+                <div className="error">{formik.errors.firstName}</div>
+              )}
             </Form.Group>
             <Form.Group>
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your full name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter your full name"
+                name="lastName"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.lastName && formik.errors.lastName && (
+                <div className="error">{formik.errors.lastName}</div>
+              )}
             </Form.Group>
             <Form.Group>
               <Form.Label>Enter your email address:</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="Enter your your email address"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+              {formik.touched.email && formik.errors.email && (
+                <div className="error">{formik.errors.email}</div>
+              )}
             </Form.Group>
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter your password" />
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.password && formik.errors.password && (
+                <div className="error">{formik.errors.password}</div>
+              )}
             </Form.Group>
             <a href="/login">Already have an account? Login here</a>
             <Button variant="primary" type="submit">
